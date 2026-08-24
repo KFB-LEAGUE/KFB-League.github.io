6632327ee97c9aa26724615b6906543b1be37d6d
KFB LEAGUE <smun24723@gmail.com>
2026-08-24 18:36:41 +0900

    Update daily game notices from admin panel

--- schedule-notices.js
+++ schedule-notices.js
@@ -4,13 +4,6 @@ const kfbScheduleNotices = [
     "date": "2026-08-24",
     "time": "19:30",
     "title": "round 7",
-    "match": "samsung @ kt server 1",
-    "content": ""
-  },
-  {
-    "date": "2026-08-24",
-    "time": "19:30",
-    "title": "round 7",
     "match": "yakult @ hyundai server 2",
     "content": ""
   }
