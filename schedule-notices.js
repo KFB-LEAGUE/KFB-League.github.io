2854e12641a245925ff14d6b2139e104e46b4444
KFB LEAGUE <smun24723@gmail.com>
2026-08-24 18:27:34 +0900

    Fix daily schedule notice global data

--- schedule-notices.js
+++ schedule-notices.js
@@ -1,10 +1,11 @@
 /* KFB League - DAILY GAME NOTICES */
-const kfbScheduleNotices = [
+// Admin에서 저장한 경기 공지 데이터입니다.
+window.kfbScheduleNotices = [
   {
     "date": "2026-08-24",
     "time": "19:30",
     "title": "round 7",
-    "match": "samsung  @ kt sever 1",
+    "match": "samsung @ kt sever 1",
     "content": ""
   },
   {
