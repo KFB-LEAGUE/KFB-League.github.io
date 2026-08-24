921daff84a496073322972dfa73e8c62eda85193
KFB LEAGUE <smun24723@gmail.com>
2026-08-24 18:54:30 +0900

    Update daily game notices from admin panel

--- schedule-notices.js
+++ schedule-notices.js
@@ -6,5 +6,12 @@ const kfbScheduleNotices = [
     "title": "라운드 7",
     "match": "삼성 @ 케이티",
     "content": "서버 1"
+  },
+  {
+    "date": "2026-08-24",
+    "time": "19:55",
+    "title": "태스트",
+    "match": "태스트",
+    "content": "태스트"
   }
 ];
