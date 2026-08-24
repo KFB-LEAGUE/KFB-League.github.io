872cf15343c7f73be6fe2d253776b44af9285b9d
KFB LEAGUE <smun24723@gmail.com>
2026-08-24 18:15:31 +0900

    Add date-based schedule notices data

---
+++ schedule-notices.js
@@ -0,0 +1,5 @@
+/* KFB League - DAILY GAME NOTICES
+ * Add and manage these from the Admin panel.
+ * Notices are shown publicly only on their selected date (Korea time).
+ */
+const kfbScheduleNotices = [];
